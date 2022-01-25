d1dc52b33f933f09a09801214652d957c2a6c54a
mertvn <mertvn@protonmail.com>
2022-01-25 19:58:45 +0300

    eslint

--- index.js
+++ index.js
@@ -6,68 +6,70 @@
 // @run-at      document-idle
 // ==/UserScript==
 
-(function() {
-  'use strict';
+function addButton(text, onclick, cssObj) {
+  const button = document.createElement('button');
+  const btnStyle = button.style;
+  document.body.appendChild(button);
+  button.innerHTML = text;
+  button.onclick = onclick;
+  btnStyle.position = 'absolute';
+  // eslint-disable-next-line no-return-assign
+  Object.keys(cssObj).forEach((key) => btnStyle[key] = cssObj[key]);
+  return button;
+}
 
-  var cssObj = {position: 'absolute', top: '25%', right:'3%', 'z-index': 3}
-  addButton("Bulk open VNDB & EGS", stuff, cssObj);
-})();
+function stuff() {
+  const openVNDB = true;
+  const openEGS = true;
 
-async function stuff(){
-var openVNDB = true;
-var openEGS = true;
-
-var page = null; // 0: VN, 1: Char
-
-if (document.location.pathname == "/v")
- page = 0;
-else if (document.location.pathname == "/c")
- page = 1;
-
-  var selection = "";
- if (page == 0)
-  selection = ".mainbox.vngrid";
- else if (page == 1)
-  selection = ".mainbox.charbgrid";
-
-var children = document.querySelector(selection).children;
-
-for (const child of children) {
-  var vndbUrl = "";
-  var egsUrl = "";
-  
-  if (page == 0){
-    var a = child.querySelector("a");
-    var href = a.href; 
-  
-    alert(href)
-    vndbUrl = href;
-    
-  
-  if (vndbUrl != "" && openVNDB)
-    GM_openInTab(vndbUrl, false);
-  
-  if (egsUrl != "" && openEGS)
-    GM_openInTab(egsUrl, false);
+  let page = null; // 0: VN, 1: Char
+  if (document.location.pathname === '/v') {
+    page = 0;
+  } else if
+  (document.location.pathname === '/c') {
+    page = 1;
   }
-  else if (page == 1){
-    {
-      alert("page1")
-      let response = await fetch(child.href);
-      alert(await response.text());
+
+  let selection = '';
+  if (page === 0) {
+    selection = '.mainbox.vngrid';
+  } else if
+  (page === 1) {
+    selection = '.mainbox.charbgrid';
+  }
+
+  const { children } = document.querySelector(selection);
+
+  // eslint-disable-next-line no-restricted-syntax
+  for (const child of children) {
+    let vndbUrl = '';
+    const egsUrl = '';
+
+    if (page === 0) {
+      const a = child.querySelector('a');
+      const { href } = a;
+
+      alert(href);
+      vndbUrl = href;
+
+      if (vndbUrl !== '' && openVNDB) {
+        GM_openInTab(vndbUrl, false);
+      }
+
+      if (egsUrl !== '' && openEGS) {
+        GM_openInTab(egsUrl, false);
+      }
+    } else if (page === 1) {
+      alert('page1');
+      const response = fetch(child.href);
+      alert(response.text());
     }
   }
-  
-  
-}
 }
 
-function addButton(text, onclick, cssObj) {
-  let button = document.createElement('button'), btnStyle = button.style
-  document.body.appendChild(button)
-  button.innerHTML = text
-  button.onclick = onclick
-  btnStyle.position = 'absolute'
-  Object.keys(cssObj).forEach(key => btnStyle[key] = cssObj[key])
-  return button
-}
+(function () {
+  const cssObj = {
+    position: 'absolute', top: '25%', right: '3%', 'z-index': 3,
+  };
+  addButton('Bulk open VNDB & EGS', stuff, cssObj);
+}());
