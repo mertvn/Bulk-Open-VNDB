f95ed1a124df39bba7aeded62ed34899f0b70a4c
mertvn <mertvn@protonmail.com>
2022-01-26 00:10:59 +0300

    mvpish

--- user.js
+++ user.js
@@ -1,5 +1,8 @@
+/* eslint-disable no-console */
+/* eslint-disable no-return-assign */
+/* eslint-disable no-alert */
 // ==UserScript==
-// @name        Bulk open VNDB
+// @name        Bulk Open VNDB
 // @include     *
 // @version     0.1
 // @grant       GM_openInTab
@@ -13,12 +16,35 @@ function addButton(text, onclick, cssObj) {
   button.innerHTML = text;
   button.onclick = onclick;
   btnStyle.position = 'absolute';
-  // eslint-disable-next-line no-return-assign
   Object.keys(cssObj).forEach((key) => btnStyle[key] = cssObj[key]);
   return button;
 }
 
-function stuff() {
+async function getEGSUrl(vndbUrl) {
+  alert('getting EGS url');
+  const response = await fetch(vndbUrl);
+  const parser = new DOMParser();
+  const doc = parser.parseFromString(await response.text(), 'text/html');
+
+  const allLangs = doc.querySelector('.mainbox.vnreleases').children;
+  console.log(allLangs);
+  const ja = Array.from(allLangs).find((v) => v.getAttribute('data-save-id') === 'vnlang-ja');
+  console.log(ja);
+  const releases = ja.querySelector('tbody');
+  console.log({ releases });
+  const abbrs = releases.querySelectorAll('abbr');
+  console.log({ abbrs });
+  const firstCompleteRelease = Array.from(abbrs).find((v) => v.getAttribute('title') === 'complete').parentElement.parentElement;
+  console.log({ firstCompleteRelease });
+  const as = firstCompleteRelease.querySelectorAll('a');
+  console.log({ as });
+  const egsUrl = Array.from(as).find((v) => v.getAttribute('href').startsWith('https://erogamescape')).href;
+  console.log(egsUrl);
+
+  return egsUrl;
+}
+
+async function stuff() {
   const openVNDB = true;
   const openEGS = true;
 
@@ -42,16 +68,32 @@ function stuff() {
 
   // eslint-disable-next-line no-restricted-syntax
   for (const child of children) {
-    let vndbUrl = '';
-    const egsUrl = '';
+    let vndbUrls = [];
 
     if (page === 0) {
       const a = child.querySelector('a');
       const { href } = a;
 
       alert(href);
-      vndbUrl = href;
+      vndbUrls.push(href);
+    } else if (page === 1) {
+      alert('page1');
+      const response = await fetch(child.href);
+      const parser = new DOMParser();
+      const doc = parser.parseFromString(await response.text(), 'text/html');
 
+      const tdAnchors = [...doc.querySelectorAll('td > a')];
+      console.log(tdAnchors);
+      const vnAnchors = Array.from(tdAnchors).filter((v) => v.getAttribute('href').startsWith('/v'));
+      console.log(vnAnchors);
+      vndbUrls = vnAnchors.map((a) => a.href);
+      console.log(vndbUrls);
+    }
+
+    // eslint-disable-next-line no-restricted-syntax
+    for (const vndbUrl of vndbUrls) {
+      const egsUrl = await getEGSUrl(vndbUrl);
+      alert('opening tabs');
       if (vndbUrl !== '' && openVNDB) {
         GM_openInTab(vndbUrl, false);
       }
@@ -59,10 +101,6 @@ function stuff() {
       if (egsUrl !== '' && openEGS) {
         GM_openInTab(egsUrl, false);
       }
-    } else if (page === 1) {
-      alert('page1');
-      const response = fetch(child.href);
-      alert(response.text());
     }
   }
 }
